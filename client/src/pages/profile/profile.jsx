cb2192cb5d52edeefc0e776f8471a5e289663f5e
ajmeraaditya02 <ajmeraaditya05@gmail.com>
2024-08-25 18:11:12 +0530

    responsive design added

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -53,7 +53,7 @@ const Profile = () => {
               </div>
               <div className="item">
                 <LanguageIcon />
-                <span>lama.dev</span>
+                <span>English</span>
               </div>
             </div>
             <button>follow</button>
